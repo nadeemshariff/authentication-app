8c8ab4f96cc1f2af2fcd787f11f14a9700a1db81
nadeemshariff <108820002+nadeemshariff@users.noreply.github.com>
2024-04-02 17:51:55 +0530

    files added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,24 +1,30 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <div className='bg-slate-400 bg-fixed'>
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+    <div className='bg-slate-200'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
-      <h1 className='font-bold font-sans'>Auth App</h1>
-      </Link>
-      <ul className='flex gap-4 '>
-      <Link to='/'>
-        <li className='hover:text-white hover:shadow-inner'>Home</li>
-      </Link>
-      <Link to='/About'>
-        <li className='hover:text-red-500 '>About</li>
-      </Link>
-      <Link to='/Sign-in'>
-        <li className='hover:text-red-500 '>SignIn</li>
-      </Link>
-      </ul>
+          <h1 className='font-bold'>Auth App</h1>
+        </Link>
+        <ul className='flex gap-4'>
+          <Link to='/'>
+            <li>Home</li>
+          </Link>
+          <Link to='/about'>
+            <li>About</li>
+          </Link>
+          <Link to='/profile'>
+            {currentUser ? (
+              <img src={currentUser.profilePicture} alt='profile' className='h-7 w-7 rounded-full object-cover' />
+            ) : (
+              <li>Sign In</li>
+            )}
+          </Link>
+        </ul>
       </div>
     </div>
-  )
+  );
 }
