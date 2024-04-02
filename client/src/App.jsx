8dbb7c244c88414795ab5c1e5ee67d8598bdb7d7
nadeemshariff <108820002+nadeemshariff@users.noreply.github.com>
2024-04-03 01:26:33 +0530

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Profile from './pages/Profile';
-import Header from './components/Header';
-import PrivateRoute from './components/PrivateRoute';
+import React from 'react'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      {/* header */}
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route element={<PrivateRoute />}>
-          <Route path='/profile' element={<Profile />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    <div className='text-red-600'>
+      App
+    </div>
+  )
 }
+
