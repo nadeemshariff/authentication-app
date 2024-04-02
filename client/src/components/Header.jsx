9036080d1482b59ce6ff4b307f8459c2f88f0c2f
nadeemshariff <108820002+nadeemshariff@users.noreply.github.com>
2024-04-02 17:36:41 +0530

    Connect to DB

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,14 +2,14 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <div className='bg-slate-200'>
+    <div className='bg-slate-400 bg-fixed'>
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to='/'>
       <h1 className='font-bold font-sans'>Auth App</h1>
       </Link>
       <ul className='flex gap-4 '>
       <Link to='/'>
-        <li className='hover:text-red-500 hover:shadow-inner'>Home</li>
+        <li className='hover:text-white hover:shadow-inner'>Home</li>
       </Link>
       <Link to='/About'>
         <li className='hover:text-red-500 '>About</li>
